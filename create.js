1c9656e2aaf7d329f8f6ba6d426024593f58d3ad
LiChangNan-Programmer <changnan666@gmail.com>
2022-03-31 20:32:52 +0800

    support create dir

--- create.js
+++ create.js
@@ -1,25 +1,32 @@
 #!/usr/bin/env node
 
+const { mkdir } = require('fs');
 let extensionName = process.argv.slice(2)[0]
 if (!extensionName) {
   throw new Error('extension name is required');
 }
 extensionName = extensionName.trim().toLowerCase();
 
-const cwd = process.cwd();
-const copy = require('copy-template-dir');
 const path = require('path');
+const cwd = path.resolve(process.cwd(), extensionName);
+const copy = require('copy-template-dir');
 const vars = { extensionName };
 const inDir = path.join(__dirname, 'templates');
 const { exec } = require('child_process');
 
-copy(inDir, cwd, vars, (err) => {
-  if (err) throw err
-  console.log('✨  Project created successfully');
-  console.log('start install dependencies...');
+mkdir(extensionName, (err) => {
+  if (err) throw err;
 
-  exec('npm install', { cwd }, (err, out) => {
+  copy(inDir, cwd, vars, (err) => {
     if (err) throw err
-    console.log(out);
+    console.log('✨  Project created successfully');
+    console.log('start install dependencies...');
+
+    exec('npm install', { cwd }, (err, out) => {
+      if (err) throw err
+      console.log(out);
+    });
   });
 });
+
+
