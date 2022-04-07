4b2a594b05ebf1aa635e4bc4bc3d949f449d48d0
123falksdfj <changnan666@gmail.com>
2022-04-07 18:03:10 +0800

    debug...

--- scripts/reload.js
+++ scripts/reload.js
@@ -7,6 +7,7 @@ const cwd = process.env.cwd;
 const packageJSON = require(path.resolve(cwd, 'package.json'));
 const name = packageJSON.name;
 
+
 const init = async () => {
   const StayFocusd = path.resolve(cwd, "./dist");
 
@@ -21,21 +22,29 @@ const init = async () => {
   });
 
   chromeExtensionPage = await browser.newPage();
-  await chromeExtensionPage.goto("chrome://extensions/");
-  await chromeExtensionPage.evaluate(() => {
-    // open development mode
-    document.querySelector("body > extensions-manager").shadowRoot.querySelector("extensions-toolbar").shadowRoot.querySelector("#devMode").click();
-  });
-
   chromeExtensionPage.on('console', async (msg) => {
     const msgArgs = msg.args();
     for (let i = 0; i < msgArgs.length; ++i) {
       console.log(await msgArgs[i].jsonValue());
     }
   });
+
+  await chromeExtensionPage.goto("chrome://extensions/");
+  await chromeExtensionPage.evaluate(() => {
+    // open development mode
+    document.querySelector("body > extensions-manager").shadowRoot.querySelector("extensions-toolbar").shadowRoot.querySelector("#devMode").click();
+  });
 }
 
 init();
+
+const reportError = () => {
+  const error = document.querySelector('extensions-manager')
+    .shadowRoot.querySelector('#viewManager')
+    .shadowRoot.querySelector('extensions-error-page')
+    .shadowRoot.querySelector('.error-message').textContent;
+  window.console.log(error);
+}
 
 const reload = () => {
   if (!chromeExtensionPage) return;
@@ -45,12 +54,26 @@ const reload = () => {
     const target = extensionsConfig.find((item) => item.name === name);
 
     if (target) {
-      document
+      const card = document
         .querySelector("extensions-manager")
         .shadowRoot.querySelector("extensions-item-list")
-        .shadowRoot.querySelector(`extensions-item[id="${target.id}"]`)
-        .shadowRoot.querySelector("cr-icon-button")
+        .shadowRoot.querySelector(`extensions-item[id="${target.id}"]`);
+
+      card.shadowRoot.querySelector('cr-icon-button[id="dev-reload-button"]')
         .click();
+
+      const buttonContainer = card.shadowRoot.querySelector('#button-strip > div:first-child');
+
+      if (buttonContainer) {
+        new MutationObserver(() => {
+          if (buttonContainer.childElementCount >= 3) {
+            buttonContainer.children[2].click();
+            // reportError();
+          }
+        }).observe(buttonContainer, { childList: true });
+      } else {
+        console.error("buttonContainer is not found");
+      }
     } else {
       console.error("plugin is not found");
     }
